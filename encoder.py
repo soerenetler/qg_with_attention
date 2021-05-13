9b3f51db29bf6e181b3d36a87c85fa6be2c0b970
soerenetler <soeren.etler@gmail.com>
2021-05-13 11:12:37 +0200

    remove batch_sz from encoder

--- encoder.py
+++ encoder.py
@@ -5,7 +5,6 @@ class Encoder(tf.keras.Model):
     def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz, embedding_matrix, bidirectional=False):
         super(Encoder, self).__init__()
         self.bidirectional = bidirectional
-        self.batch_sz = batch_sz
         self.enc_units = enc_units
 
         self.embedding = tf.keras.layers.Embedding(vocab_size,
