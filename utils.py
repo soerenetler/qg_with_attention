e5495ec409abbaf4309223d8a2a91872e571992d
soerenetler <soeren.etler@gmail.com>
2021-05-13 17:44:24 +0200

    more prints

--- utils.py
+++ utils.py
@@ -48,10 +48,13 @@ def loss_function(real, pred):
     print("loss - real.shape", real.shape)
     print("loss - pred.shape", pred.shape)
     mask = tf.math.logical_not(tf.math.equal(real, 0))
+    print("loss - mask.shape", mask.shape)
     loss_ = loss_object(real, pred)
-
+    print("loss - loss_.shape", loss_.shape)
     mask = tf.cast(mask, loss_.dtype)
     loss_ *= mask
+
+    print("loss - loss_.shape", loss_.shape)
 
     return tf.reduce_mean(loss_)
 
