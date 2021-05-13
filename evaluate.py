076678f2263694c8314f59b421f3477ba0289d7c
soerenetler <soeren.etler@gmail.com>
2021-05-13 19:25:39 +0200

    typo

--- evaluate.py
+++ evaluate.py
@@ -99,7 +99,7 @@ class QuestionGenerator(tf.keras.Model):
             decoder_embedding_matrix = self.decoder.embedding.variables[0]
             print("decoder_embedding_matrix: ", decoder_embedding_matrix.shape)
 
-            outputs, final_state, sequence_lengths = self.decoder.inferance_decoder(decoder_embedding_matrix, start_tokens = start_tokens, end_token= end_token, initial_state=decoder_initial_state)
+            outputs, final_state, sequence_lengths = self.decoder.inference_decoder(decoder_embedding_matrix, start_tokens = start_tokens, end_token= end_token, initial_state=decoder_initial_state)
             print("final_state, ", final_state)
             print("final_state.alignment_history, ", final_state.alignment_history)
             print("final_state.alignment_history.stack(), ", final_state.alignment_history.stack())
