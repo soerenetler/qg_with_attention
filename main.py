60b93f56bb3e00d699ccf690d282023a7cd75ac7
soerenetler <soeren.etler@gmail.com>
2021-05-19 12:08:28 +0200

    try again

--- main.py
+++ main.py
@@ -180,19 +180,19 @@ qg.fit(dataset, epochs=EPOCHS, batch_size=BATCH_SIZE, callbacks=[
 # qg.save(path_to_model+"saved_model/")
 
 qg.translate([['two', 'months', 'later', 'the', 'band', 'got', 'signed', 'to', 'a', 'three',
-             'album', 'deal', 'with', ',', 'which', 'left', '.']], attention_plot_folder=path_to_model)
+             'album', 'deal', 'with', ',', 'which', 'left', '.']]*64, attention_plot_folder=path_to_model)
 qg.translate([["Golm", "is", "a", "locality", "of", "Potsdam", ",", "the", "capital", "of",
-             "the", "German", "state", "of", "Brandenburg", "."]], attention_plot_folder=path_to_model)
-qg.translate(["the largest of these is the eldon square shop-ping centre , one of the largest city centre shopping com-plexes in the uk .".split(" ")],
+             "the", "German", "state", "of", "Brandenburg", "."]]*64, attention_plot_folder=path_to_model)
+qg.translate(["the largest of these is the eldon square shop-ping centre , one of the largest city centre shopping com-plexes in the uk .".split(" ")]*64,
              attention_plot_folder=path_to_model)
 
 qg.translate(
-    ["the largest of these is the eldon square shop-ping centre , one of the largest city centre shopping com-plexes in the uk .".split(" ")], beam_width=3)
+    ["the largest of these is the eldon square shop-ping centre , one of the largest city centre shopping com-plexes in the uk .".split(" ")]*64, beam_width=3)
 qg.translate([['Golm', 'is', 'a', 'locality', 'of', 'Potsdam', ',', 'the',
-              'capital', 'of', 'the', 'German', 'state', 'of', 'Brandenburg', '.']], beam_width=3)
+              'capital', 'of', 'the', 'German', 'state', 'of', 'Brandenburg', '.']]*64, beam_width=3)
 
 dev_sentences, dev_questions = qg_dataset.create_dataset(qg_dataset.dev_path)
-chunks = [dev_sentences[x:x+100] for x in range(0, len(dev_sentences), 100)]
+chunks = [dev_sentences[x:x+64] for x in range(0, len(dev_sentences), 64)]
 for chunk in chunks:
     outputs = qg.translate(chunk, beam_width=3)
 
