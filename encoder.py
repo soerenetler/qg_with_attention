76412edbf4a4def87dd343672bb5ef91ba315465
soerenetler <soeren.etler@gmail.com>
2021-05-24 00:42:35 +0200

    fix ecoder states output

--- encoder.py
+++ encoder.py
@@ -47,8 +47,11 @@ class Encoder(tf.keras.layers.Layer):
             x, training=training)  # , initial_state=hidden)
         output = result[0]
         print("Encoder result:", result[1:])
-        states = tuple([tf.concat(result[i:i+2], 1)
-                       for i in range(1, len(result[1:]), 2)])
+        if self.bidirectional:
+            states = tuple([tf.concat(result[i:i+2], 1)
+                        for i in range(1, len(result[1:]), 2)])
+        else:
+            tuple(result[1:])
         print("Encoder state:", states)
         if len(states) == 1:
             return output, states[0]
