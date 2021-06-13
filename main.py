f82c09abab27272522ca59955bc012e31ce560ad
soerenetler <soeren.etler@gmail.com>
2021-06-13 16:12:51 +0200

    add quac & tedq

--- main.py
+++ main.py
@@ -268,3 +268,45 @@ for ans_sent_chunk, ans_token_chunk in zip(ans_sent_chunks, ans_token_chunks):
         for output in outputs:
             f.write(str(output))
             f.write('\n')
+
+if dataset == "squad":
+    test_quac_ans_sent, test_quac_ans_token, test_quac_questions = qg_dataset.create_dataset(qg_dataset.test_quac_path)
+    ans_sent_chunks = [test_quac_ans_sent[x:x+64] for x in range(0, len(test_quac_ans_sent), 64)]
+    ans_token_chunks = [test_quac_ans_token[x:x+64] for x in range(0, len(test_quac_ans_token), 64)]
+    for ans_sent_chunk, ans_token_chunk in zip(ans_sent_chunks, ans_token_chunks):
+        outputs = qg.translate(ans_sent_chunk, ans_token_chunk, beam_width=3)
+
+        filename = "quac_test.txt"
+
+        with open(path_to_model + filename, "a") as f:
+            for output in outputs:
+                f.write(str(output))
+                f.write('\n')
+elif dataset == "quac":
+    test_squad_ans_sent, test_squad_ans_token, test_squad_questions = qg_dataset.create_dataset(qg_dataset.test_squad_path)
+    ans_sent_chunks = [test_squad_ans_sent[x:x+64] for x in range(0, len(test_squad_ans_sent), 64)]
+    ans_token_chunks = [test_squad_ans_token[x:x+64] for x in range(0, len(test_squad_ans_token), 64)]
+    for ans_sent_chunk, ans_token_chunk in zip(ans_sent_chunks, ans_token_chunks):
+        outputs = qg.translate(ans_sent_chunk, ans_token_chunk, beam_width=3)
+
+        filename = "squad_test.txt"
+
+        with open(path_to_model + filename, "a") as f:
+            for output in outputs:
+                f.write(str(output))
+                f.write('\n')
+    
+
+
+test_tedq_ans_sent, test_tedq_ans_token, test_tedq_questions = qg_dataset.create_dataset(qg_dataset.test_tedq_path)
+ans_sent_chunks = [test_tedq_ans_sent[x:x+64] for x in range(0, len(test_tedq_ans_sent), 64)]
+ans_token_chunks = [test_tedq_ans_token[x:x+64] for x in range(0, len(test_tedq_ans_token), 64)]
+for ans_sent_chunk, ans_token_chunk in zip(ans_sent_chunks, ans_token_chunks):
+    outputs = qg.translate(ans_sent_chunk, ans_token_chunk, beam_width=3)
+
+    filename = "tedq_test.txt"
+
+    with open(path_to_model + filename, "a") as f:
+        for output in outputs:
+            f.write(str(output))
+            f.write('\n')
